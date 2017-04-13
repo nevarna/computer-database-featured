33784adad6a05f8529c32b19893658ee9299f99e
Valloris Cylly <vcylly@excilys.com>
2017-04-13 14:15:52 +0200

    {fixDefaut} ajout fonc show(name),suivantShow() & chgt args fonc [pck persistence(DAOComputer.java)]

--- src/com/navarna/computerdb/persistence/DAOComputer.java
+++ src/com/navarna/computerdb/persistence/DAOComputer.java
@@ -1,14 +1,18 @@
 package com.navarna.computerdb.persistence;
 
+import com.navarna.computerdb.model.Computer;
+
 public interface DAOComputer {
 	
 	public int getPage() ;
 	public int getNbElement () ;
-	public boolean insert(String information) ;
-	public boolean update (int id , String information) ;
-	public boolean delete (int id) ; 
-	public boolean show (int id); 
+	public boolean insert(Computer computer) ;
+	public boolean update (Computer computer) ;
+	public boolean delete (long id) ; 
+	public boolean show (long id) ; 
+	public boolean show (String name) ;
 	public boolean list () ;
 	public void resetList() ;
 	public boolean suivantList() ;
+	public boolean suivantShow(String name) ; 
 }
