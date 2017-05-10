f4e7c85c90c1ce2bf60a16e1fcc841bdc8426e9b
Valloris Cylly <vcylly@excilys.com>
2017-05-10 18:01:43 +0200

    {feature} script corriger [pck none]

--- script/jenkins.sh
+++ script/jenkins.sh
@@ -1,4 +1,4 @@
-docker run -d --name jenkins -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -p 8080:8080 jenkins_docker:test
+docker run -d --name jenkins -u root -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v maven-jenkins:/myData -v tomcat-jenkins:/tomcat -p 8080:8080 jenkins
 echo "Attente 20 secondes"
 /bin/sleep 20
 docker exec jenkins /bin/sh -c "cd var/jenkins_home/secrets/;cat initialAdminPassword;exit"
